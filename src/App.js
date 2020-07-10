75d5cc38d2ea7a69ce52098c28e273c310ec20ec
Gautham P Krishnan <gauthamkrishnanp@gmail.com>
2020-07-10 10:05:12 +0530

    The UI part is done!

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Game from './components/Game'
 import './App.css';
 
 class App extends React.Component{
   render(){
     return(
-      <div>Hello Everyone </div>
+     <Game/>
     )
   }
 }
